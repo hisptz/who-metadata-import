780685c836002182497ec75a6cd74dd92755b5d6
moseBunango <bunangomoses@gmail.com>
2023-09-13 15:05:30 +0300

    begin aligment button

---
+++ src/AlignmentReport.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from '@dhis2/ui'
+
+const AlignmentReport=()=>{
+    return(
+        <Button></Button>
+    );
+}
+export default AlignmentReport;
--- src/DetailsPage.tsx
+++ src/DetailsPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { metadataData } from './metadataConfigurations';
+import { Button } from '@dhis2/ui'
 
 const DetailsPage = () => {
   const { title } = useParams();
@@ -10,20 +11,20 @@ const DetailsPage = () => {
   const categoryDetails = metadataData.find((category) => category.title === title);
 
   return (
-    <div className="container mt-4">
+    <div className="container mt-4" style={{ paddingLeft: '150px', paddingRight: '150px' }}>
       <div className="row">
         <div className="col-md-6">
-          <h1>{categoryDetails.title}</h1>
-          <p>{categoryDetails.description}</p>
+          <h1>{categoryDetails?.title}</h1>
+          <p>{categoryDetails?.description}</p>
         </div>
         <div className="col-md-6">
-          <img src={categoryDetails.image} alt={categoryDetails.title} className="img-fluid" />
+          <img src={categoryDetails?.image} alt={categoryDetails?.title} className="img-fluid" />
         </div>
       </div>
       <div className="row mt-4">
         <div className="col-md-12">
           <h2>Packages</h2>
-          {categoryDetails.packages.map((pkg, index) => (
+          {categoryDetails?.packages.map((pkg, index) => (
             <div key={index} className="mb-3">
               <h3>{pkg.title}</h3>
               <p>{pkg.description}</p>
@@ -37,18 +38,33 @@ const DetailsPage = () => {
     </tr>
   </thead>
   <tbody>
-     <tr>
-          <td>{pkg.dhis2Version}</td>
-          <td>{pkg.packageVersion}</td>
-          <td><a href={pkg.resources}>Resources</a></td>
-        </tr>
-  </tbody>
+                  {pkg.config.map((config, configIndex) => (
+                    <tr key={configIndex}>
+                      <td>{config.dhis2Version}</td>
+                      <td>{config.packageVersion}</td>
+                      <td><a href={config.resources}><Button>Resources</Button></a></td>
+                    </tr>
+                  ))}
+                </tbody>
   </table>
               {/* Add more details about the package if needed */}
             </div>
           ))}
         </div>
       </div>
+      <div className="row mt-4">
+        <div className="col-md-12 text-center">
+      <Button
+    name="Primary button"
+    onClick={() => window.alert('It works!')}
+    primary
+    value="default"
+>
+    Begin Alignment
+</Button>
+    </div>
+    </div>
+    <div className="mt-4"></div>
     </div>
   );
 };
